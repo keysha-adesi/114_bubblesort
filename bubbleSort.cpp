e434e09f8f784b4fca6ae2c98c7a0b7a1bb6104d
keysha-adesi <keyshaputrii590@gmail.com>
2026-03-05 11:40:06 +0700

    menampilkan array

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -49,3 +49,23 @@ void bublesortAraay() {   //procedur untuk mengurutkan array dengan
 
      } while (pass <= n-1);  // step 5
 }
+
+
+ void display() {
+    cout << endl;
+    cout << "================================" << endl;
+    cout << "Element array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for ( int j = 0; j < n; j++) {
+        cout << arr[j]; // menampilkan array 
+        if (j< n - 1) {
+            cout << " --> ";
+        }
+    }
+cout << endl;
+cout << endl;
+cout <<" jumlah pass = " <<n - 1 << endl; //menampilkan jumlah  dari pass 
+cout << endl;
+cout << endl;
+ }

5f7fa31a04130ebfae356975e210045c98922542
keysha-adesi <keyshaputrii590@gmail.com>
2026-03-05 10:43:48 +0700

    menambahkan libbrary

---
+++ bubble.Sort.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20]; //Deklarasi variable global arry a dengan ukuran 20
+int n; //Deklarasi variable global n untuk menyimpan banyak elemen 
